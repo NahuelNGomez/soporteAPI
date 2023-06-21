0d2a60be725e97ac6a017c5c13166f9c25f08f83
Alexis094 <72041462+Alexis094@users.noreply.github.com>
2023-06-20 22:02:43 -0300

    se actualiza readme, se agrega script de ejecucion en windows y se actualiza gitignore

---
+++ README.md
@@ -0,0 +1,19 @@
+# Requisitos
+* Python 3.10
+ 
+## Para correr la app:
+
+### Instalar los requisitos:
+`` pip install -r requirements.txt``
+
+### Correr la app:
+`` uvicorn app:app``
+
+### Acceder a FastApi:
+`` https://apisoporte.onrender.com/docs``
+
+#### Correr test de cucumber:
+`` behave test\features``
+
+## Ejecución por script en windows 10:
+`` ./run.ps1``
---
+++ run.ps1
@@ -0,0 +1,11 @@
+echo "Se instalan dependecias ..."
+pip install -r requirements.txt
+echo "Se ejecuta la app ... (recordar cerrar la consola minimizada antes de volver a ejecutar)"
+#Start-Process uvicorn app:app -NoNewWindow -Wait 
+#Start-Job -Command {uvicorn app:app}
+Start-Process powershell.exe -ArgumentList "-noexit", "-WindowStyle Minimized", -"command &{uvicorn app:app}"
+sleep 10
+echo "Se corren las pruebas de bdd:"
+behave --color test\features
+echo "Se abre FastApi"
+start chrome https://apisoporte.onrender.com/docs
