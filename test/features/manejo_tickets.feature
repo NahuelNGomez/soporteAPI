637c6fa2f3bff10ab5776ff36dc6f62537a9eb0e
Nahuel Gomez <nngomez@fi.uba.ar>
2023-06-21 14:53:11 -0300

    Se añaden 2 escenarios

--- test/features/manejo_tickets.feature
+++ test/features/manejo_tickets.feature
@@ -2,10 +2,9 @@ Feature: Manejo de tickets
   Chequeando el manejo de tickets
 
 Scenario: Creacion de ticket
-    Given un cliente y un producto
+    Given un cliente y una version de producto
     When quiera informar un nuevo ticket de un producto debere informar: <"nombre ticket">, <"severidad">, <"des problema">, <"des escenario">
     Then se creará un ticket con estado "Nuevo"
-
 
 Scenario: Cambio de estado de un ticket
   Given Ticket con estado "En curso"
@@ -13,8 +12,8 @@ Scenario: Cambio de estado de un ticket
   Then Ticket no debe tener estado "En curso"
 
 
-  Scenario: Asignacion de un ticket a un producto
-    Given una lista de clientes y un producto
-    When quiera informar un nuevo ticket de un producto
-    Then deberé informar producto: versión de producto, cliente, severidad, descripción del problema, descripción del escenario
-    And se creara un ticket con estado "en curso"
+#  Scenario: Asignacion de un ticket a un producto
+#    Given una lista de clientes y un producto
+#    When quiera informar un nuevo ticket de un producto
+#    Then deberé informar producto: versión de producto, cliente, severidad, descripción del problema, descripción del escenario
+#    And se creara un ticket con estado "en curso"
--- test/steps/test_ticket_steps.py
+++ test/steps/test_ticket_steps.py
@@ -1,46 +1,87 @@
 from behave import *
 from selenium import webdriver
 from models.models import tickets
+from typing import Dict
 
 from schemes.ticket import Ticket
+from schemes.version import Version
 from schemes.producto import Producto
 from schemes.cliente import Cliente
+from service.ticketService import TicketService
 
+ticketService = TicketService()
+#ASUMO QUE SIEMPRE EXISTE UNA PRODUCTO Y  SU VERSION CON ID 2
 
 # Creacion de Ticket
-@given('un cliente y un producto')
+@given('un cliente y una version de producto')
 def un_cliente_y_un_producto(context):
-    context.cliente1 = Cliente()  #"20-4548272-9", "cliente1")
-    context.producto1 = Producto()
+    return
 
-    #cliente1 = Cliente()
-    #producto1 = Producto()
+@when(u'quiera informar un nuevo ticket de un producto debere informar: <"nombre ticket">, <"severidad">, <"des problema">, <"des escenario">')
+def step_impl(context):
+    context.ticket1 = {
+        "Nombre": "nombre ticket",
+        "Descripcion": "des_problema",
+        "Escenario": "des_escenario",
+        "Estado": "Nuevo",
+        "Severidad": "severidad",
+        "idVersion": 2,
+        "CUIT": "20-12345678-3"
+    }
+    ticketService.crearTicket(context.ticket1)
+    context.ticket1 = {"id": ticketService.getLastIdTicketAdded(),
+                    "Nombre": "nombre ticket",
+                    "Descripcion": "des_problema",
+                    "Escenario": "des_escenario",
+                    "Estado": "Nuevo",
+                    "Severidad": "severidad",
+                    "idVersion": 2,
+                    "CUIT": "20-12345678-3"
+                     }
+    
+@Then('se creará un ticket con estado "Nuevo"')
+def ticket_con_estado_nuevo(context):
+    assert(ticketService.getTicketByID(context.ticket1["id"]).Estado == "Nuevo") 
+    ticketService.deleteTicket(context.ticket1["id"])
 
-@When('quiera informar un nuevo ticket de un producto debere informar: {nombre:d}, {severidad:d}, {problema:d}, {escenario:d}')
-def quiera_informar_un_nuevo_ticket_de_producto(context, nombre, severidad, problema, escenario):
-    print(nombre)
-    print(severidad)
-    print(problema)
-    print(escenario)
 
-
-"""
 @given('Ticket con estado "En curso"')
 def ticket_con_estado_en_curso(context):
-    ticket1 = Ticket()
-
-
+    context.ticket1 ={
+                "Nombre":"TICKET_PRUEBA",
+                "Descripcion":"DESCRIPCION",
+                "Escenario":"ESCENARIO",
+                "Estado":"En curso",
+                "Severidad":"S1",
+                "idVersion":2,
+                "CUIT":"20-12345678-3"
+                  }
+    ticketService.crearTicket(context.ticket1)
+    context.ticket1 = {"id": ticketService.getLastIdTicketAdded(),
+                  "Nombre":"TICKET_PRUEBA",
+                "Descripcion":"DESCRIPCION",
+                "Escenario":"ESCENARIO",
+                "Estado":"En curso",
+                "Severidad":"S1",
+                "idVersion":2,
+                "CUIT":"20-12345678-3"
+            }
 
 @when(u'Se Completa un Ticket')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When Se Completa un Ticket')
+    update_data: Dict[str, str] = {
+    "Estado": "Cerrado"
+}
+    
+    ticketService.updateTicket(context.ticket1["id"], update_data)
 
 
 @then(u'Ticket no debe tener estado "En curso"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then Ticket no debe tener estado "En curso"')
+    assert(ticketService.getTicketByID(context.ticket1["id"]).Estado != "En curso")
+    ticketService.deleteTicket(context.ticket1["id"])
 
-
+"""
 @given(u'una lista de clientes y un producto')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Given una lista de clientes y un producto')
